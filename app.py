28b1e4a84cf7c40bc5480e7a4437152229ddabd4
Jithin George <jithin.george14796@gmail.com>
2024-04-24 13:27:47 +0530

    all initial files uploaded

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def portfolio():
+    return render_template('index.html')
+
+
+
+#=== RUN APP ===#
+HOSTNAME = 'localhost'
+PORT = 5050
+if __name__ == '__main__':
+    app.run(debug=True, host=HOSTNAME,port=PORT)
